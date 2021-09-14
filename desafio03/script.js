30242a03bb13563d099415a0b1c926d5a9d87b5a
Vinicius Barriquelo <72697479+vinibarriquelo@users.noreply.github.com>
2021-09-14 13:57:37 -0300

    Ajustes do acordion, verificar tudo

--- desafio03/script.js
+++ desafio03/script.js
@@ -1,14 +1,26 @@
 function acordionList() {
     const titulos = document.querySelectorAll('dt')
+    const descricao = document.querySelectorAll('dd')
     console.log(titulos)
+
+    titulos[0].classList.add('ativo')
+    descricao[0].classList.add('ativo-block')
 
     titulos.forEach((item) => {
         item.addEventListener('click',activeClass)
     })
 
     function activeClass(){
-        this.classList.toggle('ativo')
-        this.nextElementSibling.classList.toggle('ativo')
+        titulos.forEach((item)=>{
+            item.classList.remove('ativo')
+        })
+
+        descricao.forEach((item)=>{
+            item.classList.remove('ativo-block')
+        })
+
+        this.classList.add('ativo')
+        this.nextElementSibling.classList.add('ativo-block')
     }
 }
 
